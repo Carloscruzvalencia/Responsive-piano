d2b2a9b82a2a875f4ad9314b00018f42d03f2c96
carlos <olivershtn@gmail.com>
2022-02-02 19:43:19 +0100

    added proyect files

---
+++ script.js
@@ -0,0 +1,50 @@
+var note1 = new Audio ()
+note1.src = "audio/30.mp3"
+
+var note2 = new Audio ()
+note2.src = "audio/31.mp3"
+
+var note3 = new Audio ()
+note3.src = "audio/32.mp3"
+
+var note4 = new Audio ()
+note4.src = "audio/33.mp3"
+
+var note5 = new Audio ()
+note5.src = "audio/34.mp3"
+
+var note6 = new Audio ()
+note6.src = "audio/35.mp3"
+
+var note7 = new Audio ()
+note7.src = "audio/36.mp3"
+
+var note8 = new Audio ()
+note8.src = "audio/37.mp3"
+
+var note9 = new Audio ()
+note9.src = "audio/38.mp3"
+
+var note10 = new Audio ()
+note10.src = "audio/39.mp3"
+
+var note11 = new Audio ()
+note11.src = "audio/40.mp3"
+
+var note12 = new Audio ()
+note12.src = "audio/41.mp3"
+
+var note13 = new Audio ()
+note13.src = "audio/42.mp3"
+
+var note14 = new Audio ()
+note14.src = "audio/43.mp3"
+
+var note15 = new Audio ()
+note15.src = "audio/44.mp3"
+
+var note16 = new Audio ()
+note16.src = "audio/45.mp3"
+
+var note17 = new Audio ()
+note17.src = "audio/46.mp3"
